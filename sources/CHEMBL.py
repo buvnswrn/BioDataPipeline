c0ef6e63c20567b0eaafdbfff2acff0da0da6937
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2024-06-04 20:41:43 +0200

    Added initial files with URL

---
+++ sources/CHEMBL.py
@@ -0,0 +1,49 @@
+
+CHEMBL_URL = "https://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBL-RDF/latest/"
+VERSION = 34.0
+
+ACTIVITY_FILE = CHEMBL_URL + "chembl_{0}_activity.ttl.gz".format(VERSION)
+
+ASSAY_FILE = CHEMBL_URL + "chembl_{0}_assay.ttl.gz".format(VERSION)
+
+BINDINGSITE_FILE = CHEMBL_URL + "chembl_{0}_bindingsite.ttl.gz".format(VERSION)
+
+BIOCMPT_FILE = CHEMBL_URL + "chembl_{0}_biocmpt.ttl.gz".format(VERSION)
+
+CELL_FILE = CHEMBL_URL + "chembl_{0}_cellline.ttl.gz".format(VERSION)
+
+COMPLEX_TARGET_TARGET_CMPT_FILE = CHEMBL_URL + "chembl_{0}_complextarget_targetcmpt_ls.ttl.gz.ttl.gz".format(VERSION)
+
+DOCUMENT_FILE = CHEMBL_URL + "chembl_{0}_document.ttl.gz".format(VERSION)
+
+GROUP_TARGET_FILE = CHEMBL_URL + "chembl_{0}_grouptargettargetcmpt_ls.ttl.gz".format(VERSION)
+
+INDICATION_FILE = CHEMBL_URL + "chembl_{0}_indication.ttl.gz".format(VERSION)
+
+JOURNAL_FILE = CHEMBL_URL + "chembl_{0}_journal.ttl.gz".format(VERSION)
+
+MOA_FILE = CHEMBL_URL + "chembl_{0}_moa.ttl.gz".format(VERSION)
+
+MOLECULE_FILE = CHEMBL_URL + "chembl_{0}_molecule.ttl.gz".format(VERSION)
+
+MOLECULE_CHEBI_FILE = CHEMBL_URL + "chembl_{0}_molecule_chebi_ls.ttl.gz".format(VERSION)
+
+MOLHIERARCHY_FILE = CHEMBL_URL + "chembl_{0}_molhierarchy.ttl.gz".format(VERSION)
+
+PROTCLASS_FILE = CHEMBL_URL + "chembl_{0}_protclass.ttl.gz".format(VERSION)
+
+SINGLE_TARGET_TARGETCMPT_FILE = CHEMBL_URL + "chembl_{0}_singletarget_targetcmpt_ls.ttl.gz".format(VERSION)
+
+SOURCE_FILE = CHEMBL_URL + "chembl_{0}_source.ttl.gz".format(VERSION)
+
+TARGET_FILE = CHEMBL_URL + "chembl_{0}_target.ttl.gz".format(VERSION)
+
+TARGETCMPT_FILE = CHEMBL_URL + "chembl_{0}_targetcmpt.ttl.gz".format(VERSION)
+
+TARGETCMPT_UNIPROT_FILE = CHEMBL_URL + "chembl_{0}_targetcmpt_uniprot_ls.ttl.gz".format(VERSION)
+
+TARGET_REL_FILE = CHEMBL_URL + "chembl_{0}_targetrel.ttl.gz".format(VERSION)
+
+UNICHEM_FILE = CHEMBL_URL + "chembl_{0}_unichem.ttl.gz".format(VERSION)
+
+VOID_FILE = CHEMBL_URL + "void.ttl.gz"
---
+++ sources/NPass.py
@@ -0,0 +1,13 @@
+
+GENERAL_INFORMATION_URL = "https://bidd.group/NPASS/downloadFiles/NPASSv2.0_download_naturalProducts_generalInfo.txt"
+
+STRUCTURE_URL = "https://bidd.group/NPASS/downloadFiles/NPASSv2.0_download_naturalProducts_structureInfo.txt"
+
+ACTIVITY_RECORDS_URL = "https://bidd.group/NPASS/downloadFiles/NPASSv2.0_download_naturalProducts_activities.txt"
+
+SPECIES_SOURCE_URL = "https://bidd.group/NPASS/downloadFiles/NPASSv2.0_download_naturalProducts_species_pair.txt"
+
+TARGETS_URL = "https://bidd.group/NPASS/downloadFiles/NPASSv2.0_download_naturalProducts_targetInfo.txt"
+
+SPECIES_TAXONOMIC_INFO_URL = "https://bidd.group/NPASS/downloadFiles/NPASSv2.0_download_naturalProducts_targetInfo.txt"
+
