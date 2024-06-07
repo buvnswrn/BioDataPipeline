31c09bba62289ddd65e98bfd81017625f3339200
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2024-06-07 14:26:53 +0200

    Added source file URLs

---
+++ data/source/NAEB.py
@@ -0,0 +1,14 @@
+
+
+SOURCES_PATH = 'naeb_dump/csv/sources.csv'
+
+SPECIES_PATH = 'naeb_dump/csv/species.csv'
+
+TRIBES_PATH = 'naeb_dump/csv/tribes.csv'
+
+USE_CATEGORIES_PATH = 'naeb_dump/csv/use_categories.csv'
+
+USE_SUBCATEGORY_PATH = 'naeb_dump/csv/use_subcategories.csv'
+
+USES_PATH = 'naeb_dump/csv/uses.csv'
+
