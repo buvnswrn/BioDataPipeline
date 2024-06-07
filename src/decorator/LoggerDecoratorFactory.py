05995ad8a7916c060d41b8f82df73c0887d609e6
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2024-06-07 22:36:28 +0200

    Added loggers

--- src/decorator/LoggerDecoratorFactory.py
+++ src/decorator/LoggerDecoratorFactory.py
@@ -7,6 +7,11 @@ class LoggerDecoratorFactory:
     Factory class to create logger time decorators
     """
 
+    type = None
+
+    def __init__(self, type):
+        LoggerDecoratorFactory.type = type
+
     @classmethod
     def log_time(cls, func):
         """
@@ -20,7 +25,7 @@ class LoggerDecoratorFactory:
             start_time = time.time()
             result = func(*args, **kwargs)
             end_time = time.time()
-            logging.info(f"Time taken by {func.__qualname__}: {end_time - start_time} seconds")
+            logging.info(f"{cls.type}: Time taken by {func.__qualname__}: {end_time - start_time} seconds")
             return result
 
         return wrapper
--- src/pipeline/Extractor.py
+++ src/pipeline/Extractor.py
@@ -1,20 +1,25 @@
 import sqlite3
 
 import pandas as pd
+from src.decorator.LoggerDecoratorFactory import LoggerDecoratorFactory
+
+logger = LoggerDecoratorFactory.log_time
 
 
 class Extractor:
     def __init__(self, config: dict):
         self.config = config
 
+    @logger
     def extract(self, data) -> pd.DataFrame:
-        raise NotImplementedError
+        raise NotImplementedError("Extract method not implemented")
 
 
 class NPASSExtractor(Extractor):
     def __init__(self, path):
         super().__init__({"path": path})
 
+    @logger
     def extract(self, data) -> pd.DataFrame:
         file_path = self.config["path"]
         return pd.read_csv(file_path, sep='\t', encoding="ISO-8859-1")
@@ -26,6 +31,7 @@ class NAEBExtractor(Extractor):
             self.conn = sqlite3.connect(path)
         super().__init__({"path": path, "csv": csv})
 
+    @logger
     def extract(self, table_name=None) -> pd.DataFrame:
         file_path = self.config["path"]
         if self.config["csv"]:
@@ -39,5 +45,6 @@ class ChEMBLExtractor(Extractor):
         self.conn = sqlite3.connect(path)
         super().__init__({"path": path})
 
+    @logger
     def extract(self, table_name=None) -> pd.DataFrame:
         return pd.read_sql(table_name, self.conn)
